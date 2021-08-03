d808b57779276592ca2b2bdd60d6ef9c89a26815
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2021-08-03 21:12:36 +0300

    Parse absolute file path correctly on Windows.

--- src/enhanceYargs.mjs
+++ src/enhanceYargs.mjs
@@ -42,6 +42,7 @@ const enhanceYargs = ({
   ...restArgs
 }) => {
   const files = _.length > 0 ? _ : args.slice(1);
+
   try {
     if (files.length === 0)
       throw new ApplicationError(
@@ -54,10 +55,22 @@ For example: ftnode.mjs --unicodes AA FF -- ./file1.ttf ./file2.ttf\n`
     else printError(error);
     process.exit(1);
   }
+
   const axisLoc = toAxisLoc(restArgs);
 
+  // If we are on Windows and the paths are absolute starting with '/c/' or '/C/',
+  // transform them to 'C:/'.
+  const transformedFiles = files.map((file) => {
+    if (process.platform === "win32" && /^\/[a-z]\//.test(file)) {
+      return file.replace(/\/([a-z])\//, (match, offset, string) => {
+        const result = `${offset.toUpperCase()}:/`;
+        return result;
+      });
+    } else return file;
+  });
+
   const parsedArgs = {
-    files,
+    files: transformedFiles,
     verbose,
     pyftsubset,
     varLibInstancer: varlibinstancer,
